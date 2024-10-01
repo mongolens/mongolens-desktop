c515fd7d812ea9bbdc707f4b9e35c3707b39314b
Hamdi Tuğ <hamditugmobil@gmail.com>
2024-10-02 00:30:51 +0300

    tailwind added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   extends: 'erb',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off', // React 17 ve üstü için gereksiz
+    'react/jsx-filename-extension': 'off', // JSX dosya uzantı kuralını kapat
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/no-import-module-exports': 'off',
@@ -13,6 +13,13 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function', // Named components as function declarations
+        defaultComponents: 'arrow',   // Default components as arrow functions
+      },
+    ],
   },
   parserOptions: {
     ecmaVersion: 2022,
@@ -20,7 +27,6 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
         moduleDirectory: ['node_modules', 'src/'],
---
+++ src/renderer/views/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingView: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Merhaba, Tailwind CSS çalışıyor!</h1>
+    </div>
+  );
+};
+
+export default LandingView;
